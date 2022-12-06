8666e4c8aed3b8833513a0d35e5b0eadbdc88bec
FreezyLemon <freezylemon@proton.me>
2022-12-06 20:33:03 +0100

    day 6 part 1

---
+++ day06/src/main.rs
@@ -0,0 +1,33 @@
+use std::collections::VecDeque;
+
+mod input;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = crate::input::get_input()?;
+    let mut input = input.chars();
+    let n = 4;
+
+    let mut buf = VecDeque::with_capacity(n);
+    for _ in 0..n {
+        let val = input.next().expect("input has at least n characters");
+        buf.push_back(val);
+    }
+
+    let mut result = 0;
+    for (idx, c) in input.enumerate() {
+        let duplicates = (1..buf.len())
+            .any(|i| buf.range(i..).any(|&t| t == buf[i - 1]));
+        
+        if !duplicates {
+            result = idx + n;
+            break;
+        }
+        
+        buf.push_back(c);
+        buf.pop_front();
+    }
+
+    println!("result: {result}");
+
+    Ok(())
+}

85a6cd05a22bb7d376aa40882066897fa795331f
FreezyLemon <freezylemon@proton.me>
2022-12-06 18:24:35 +0100

    day 5 part 1

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -2,7 +2,80 @@ mod input;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = crate::input::get_input()?;
-    
+    let lines = input.split('\n');
+    let (instructions, crates) = lines.partition::<Vec<_>, _>(|l| l.starts_with('m'));
+
+    let mut iter = crates.into_iter().rev().skip(1);
+    let pos_line = iter.next().unwrap();
+    let stack_lines = iter.collect::<Vec<_>>();
+
+    // prepare stacks and char positions
+    let mut stacks = pos_line.split_whitespace()
+        .map(|idx| pos_line.find(idx).unwrap())
+        .map(|idx| (idx, Vec::<char>::with_capacity(stack_lines.len())))
+        .map(|(idx, mut vec)| {
+            let mut items: Vec<char> = stack_lines
+                .iter()
+                .map(|l| l.chars().nth(idx).unwrap())
+                .take_while(|c| *c != ' ')
+                .collect();
+            
+            vec.append(&mut items);
+            vec
+        })
+        .collect::<Vec<_>>();
+
+    let instructions = instructions
+        .into_iter()
+        .map(|l| Instruction::parse(l));
+
+    for inst in instructions {
+        for _ in 0..inst.count {
+            let val = {
+                let stack = &mut stacks[inst.from - 1];
+                stack.pop().expect("Stack has elements")
+            };
+
+            let to = &mut stacks[inst.to - 1];
+            to.push(val)
+        }
+    }
+
+    let result = stacks
+        .into_iter()
+        .map(|v| v.last().unwrap().to_string())
+        .reduce(|acc, item| acc + &item)
+        .unwrap();
+
+    println!("result: {result}");
 
     Ok(())
+}
+
+struct Instruction {
+    from: usize,
+    to: usize,
+    count: usize,
+}
+
+impl Instruction {
+    fn parse(s: &str) -> Self {
+        let mut split = s.split_whitespace();
+        assert_eq!(split.next(), Some("move"));
+        let count = split.next().unwrap().parse().unwrap();
+
+        assert_eq!(split.next(), Some("from"));
+        let from = split.next().unwrap().parse().unwrap();
+
+        assert_eq!(split.next(), Some("to"));
+        let to = split.next().unwrap().parse().unwrap();
+
+        assert_eq!(split.next(), None);
+
+        Instruction {
+            from,
+            to,
+            count,
+        }
+    }
 }

466191bdcdeb80eb5281cba05225caf8e57d6144
FreezyLemon <freezylemon@proton.me>
2022-12-06 16:43:28 +0100

    day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -5,12 +5,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if let Some(input_file) = args.next() {
         let inventory = std::fs::read_to_string(input_file)?;
+
+        let lines = inventory.split("\n");
+        let iter = ThreeLineIter { split: lines };
         
-        let a: i32 = inventory
-            .split("\n")
-            .map(|line| line.split_at(line.len() / 2))
-            .map(|(c1, c2)| {
-                c1.chars().find(|c| c2.contains(*c)).expect("compartments have shared item type")
+        let a: i32 = iter.map(|lines| {
+                lines[0]
+                    .chars()
+                    .filter(|c| lines[1].contains(*c))
+                    .find(|c| lines[2].contains(*c))
+                    .expect("can find shared item type")
             })
             .map(|c| {
                 if c.is_lowercase() {
@@ -28,4 +32,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+struct ThreeLineIter<'a> {
+    split: core::str::Split<'a, &'a str>,
+}
+
+impl<'a> Iterator for ThreeLineIter<'a> {
+    type Item = [&'a str; 3];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let line1 = self.split.next();
+
+        if let Some(line) = line1 {
+            Some([
+                line,
+                self.split.next().unwrap(),
+                self.split.next().unwrap(),
+            ])
+        } else {
+            None
+        }
+    }
 }

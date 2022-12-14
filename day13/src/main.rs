6a074f2f79379e60bce4981081f48e22bc1cf170
FreezyLemon <freezylemon@proton.me>
2022-12-14 01:46:35 +0100

    day 13 part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -5,14 +5,26 @@ use element::Element;
 
 fn main() {
     let content = crate::input::get_input().unwrap();
-    let index_sum: usize = content
-        .split("\n\n")
-        .map(|block| block.split_once('\n').expect("block has 2 lines"))
-        .map(|(l, r)| (l.parse::<Element>().unwrap(), r.parse::<Element>().unwrap()))
-        .enumerate()
-        .filter(|(_, (l, r))| l < r)
-        .map(|(idx, _)| idx + 1)
-        .sum();
+    let mut elements: Vec<Element> = content
+        .split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse().expect("can parse as packet"))
+        .collect();
 
-    println!("result: {index_sum}");
+    elements.push("[2]]".parse().unwrap());
+    elements.push("[6]]".parse().unwrap());
+
+    elements.sort_unstable();
+
+    let div1: Element = "[2]]".parse().unwrap();
+    let div2: Element = "[6]]".parse().unwrap();
+
+    let decoder_key = elements.into_iter()
+        .enumerate()
+        .filter(|(_, p)| p == &div1 || p == &div2)
+        .map(|(idx, _)| idx + 1)
+        .reduce(|acc, elem| acc * elem)
+        .expect("has elements");
+    
+    println!("result: {decoder_key}");
 }

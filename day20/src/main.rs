d290bccf96e61d4603900d40f67cf61161642f7c
FreezyLemon <freezylemon@proton.me>
2023-02-04 20:57:50 +0100

    day 20 part 2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -3,25 +3,27 @@ mod input;
 fn main() {
     let content = crate::input::get_input().unwrap();
 
-    let mut encrypted: Vec<(usize, i16)> = content
+    let mut encrypted: Vec<(usize, i64)> = content
         .split('\n')
         .map(|line| line.parse().unwrap())
+        .map(|v: i64| v * 811589153)
         .enumerate()
         .collect();
 
     let l = encrypted.len();
 
-    for idx in 0..l {
-        let idx = index_of(&encrypted, |(oi, _)| *oi == idx).unwrap();
-        let elem = encrypted.remove(idx);
-        let offset = elem.1.rem_euclid(l as i16 - 1);
-
-        let new_idx = (idx as i16 + offset) as usize % (l - 1);
-
-        encrypted.insert(new_idx, elem);
+    // mix
+    for _ in 0..10 {
+        for idx in 0..l {
+            let idx = index_of(&encrypted, |(oi, _)| *oi == idx).unwrap();
+            let elem = encrypted.remove(idx);
+            let offset = elem.1.rem_euclid(l as i64 - 1);
+    
+            let new_idx = (idx as i64 + offset) as usize % (l - 1);
+    
+            encrypted.insert(new_idx, elem);
+        }
     }
-
-    println!("{encrypted:?}");
 
     let zero_idx = index_of(&encrypted, |(_, val)| *val == 0).unwrap();
     let first_idx = (zero_idx + 1000) % l;
